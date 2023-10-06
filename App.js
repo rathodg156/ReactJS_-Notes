1a04280be42f860db515176ffb8debb6b1d09d2b
rathodg156 <rathodg156@gmail.com>
2023-10-06 15:26:47 +0530

    Episode-02

--- App.js
+++ App.js
@@ -1,3 +1,18 @@
-const heading=React.createElement("h1",{abc:"xyz"},"Hello World From React")
-const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+
+const parent=React.createElement("h1",{id:"parent"},["Hello World From React 🚀 🚀",React.createElement("div",{id:"child"},"Hello World From child component")])
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent)
+
+// step to import or create a React Project steps are 
+// 1) just add CDN Links to the project inside Body tag 
+//  2) then create your code in script tag or in separate js file and import it in attribute of Script tag in index.html file 
+// 3) then in script file now you have to create a element like did in java script so fro this we use React.createElement("element tag ",{object value as an attribute to that tag },"value in element tag to be view") store that into heading variable 
+// 4) then we have to create root and get that element on document as root , for that we use ReactDOM library of ract 
+// 5 ) const root = ReactDOM.createRoot(document.getElementById("root"))
+//6) final step is just get that root to be render as heading element
+// 7) root.render(heading)
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-// Hello Welcome to my Notes on Namaste React , It's Just what Iunderstand from the Course 
+// Hello Welcome to my Notes on Namaste React , It's Just what I understand from the Course 
+# Parcel is Beast
+- Dev Build 
+- provide us Loacal Server to host our app
+- HMR - Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling , compressing
+- Consistence Hashing
+- Code Spliting
+- Differential Bundling - to support older browser
+- Diagnostic view
+- Error Handling very much good manner
+- Https
+- Tree Shaking Algorithm - parcel remove Unused code for you , extra random code which is not in used 
+- do Transpilation
+- Web-Worker / Service Worker 
+- different dev and prod bundles
